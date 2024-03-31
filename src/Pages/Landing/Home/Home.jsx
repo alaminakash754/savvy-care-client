450dc059390cdea9be91564ee4227ceb50b7ee6b
alamin <alaminakash9754@gmail.com>
2024-03-31 13:42:55 +0600

    first commit

---
+++ src/Pages/Landing/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-green-800 "> Smile Savvy Care</h2>
+        </div>
+    );
+};
+
+export default Home;
